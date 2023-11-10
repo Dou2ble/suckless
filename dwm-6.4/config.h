ad2df37ab36880eab732c1820351d09d6784a4a1
Dou2ble <22otro@stockholmscience.se>
2023-11-10 23:48:23 +0100

    suckless

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -6,8 +6,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const int user_bh            = 26;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
-static const char *fonts[]          = { "Hack Nerd Font:size=10" };
-static const char dmenufont[]       = "Hack Nerd Font:size=10";
+static const char *fonts[]          = { "Arimo Nerd Font:size=10" };
+static const char dmenufont[]       = "Arimo Hack Nerd Font:size=10";
 
 #include "/home/otto/.cache/wal/colors-wal-dwm.h"
 
